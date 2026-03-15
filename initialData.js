40b8b3890f5c6193e1ec0e96aff06720ee53c346
lemsproject <leonardmsd@gmail.com>
2026-03-15 18:15:36 +0200

    correcting both initialdata.js and scripts.js so that they can both match

--- initialData.js
+++ initialData.js
@@ -1,47 +1,57 @@
-export const initialTasks = [
+export const initialData = [
   {
     id: 1,
     title: "Launch Epic Career 🚀",
-    description: "Create a killer Resume",
-    status: "todo",
+    description: "",
+    status: "to-do",
     board: "Launch Career",
   },
   {
     id: 2,
-    title: "Master JavaScript 💛",
-    description: "Get comfortable with the fundamentals",
-    status: "doing",
+    title: "Conquer React⚛️",
+    description: "",
+    status: "to-do",
     board: "Launch Career",
   },
   {
     id: 3,
-    title: "Keep on Going 🏆",
-    description: "You're almost there",
-    status: "doing",
-    board: "Launch Career",
-  },
-
-  {
-    id: 11,
-    title: "Learn Data Structures and Algorithms 📚",
-    description:
-      "Study fundamental data structures and algorithms to solve coding problems efficiently",
-    status: "todo",
+    title: "Understand Databases⚙️",
+    description: "",
+    status: "to-do",
     board: "Launch Career",
   },
   {
-    id: 12,
-    title: "Contribute to Open Source Projects 🌐",
-    description:
-      "Gain practical experience and collaborate with others in the software development community",
+    id: 4,
+    title: "Crush Frameworks🖼️",
+    description: "",
+    status: "to-do",
+    board: "Launch Career",
+  },
+  {
+    id: 5,
+    title: "Master JavaScript 💛",
+    description: "",
+    status: "in-progress",
+    board: "Launch Career",
+  },
+  {
+    id: 6,
+    title: "Never Give Up 🏆",
+    description: "",
+    status: "in-progress",
+    board: "Launch Career",
+  },
+  {
+    id: 7,
+    title: "Explore ES6 Features 🚀",
+    description: "",
     status: "done",
     board: "Launch Career",
   },
   {
-    id: 13,
-    title: "Build Portfolio Projects 🛠️",
-    description:
-      "Create a portfolio showcasing your skills and projects to potential employers",
+    id: 8,
+    title: "Have fun 🥳",
+    description: "",
     status: "done",
     board: "Launch Career",
   },
