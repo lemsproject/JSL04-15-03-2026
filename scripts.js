5bff9e542dc0a52dfc500e45ebd0f88993cdb9dc
lemsproject <leonardmsd@gmail.com>
2026-03-15 17:12:40 +0200

    selecting the current task in order to edit it and also initialise the application while  DOM is loading

--- scripts.js
+++ scripts.js
@@ -5,3 +5,36 @@ import { initialData } from "./initialData.js";
  * @type {Array<Object>}
  */
 let tasks = [...initialData];
+
+/**
+ * Tracks the ID of the currently selected task for editing
+ * @type {number|null}
+ */
+let selectedTaskId = null;
+
+/**
+ * Initialize the application when DOM is loaded
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  renderTasks();
+  setupModalEvents();
+});
+/**
+ * Render all tasks into their correct columns based on status
+ */
+function renderTasks() {
+  clearColumns();
+
+  tasks.forEach((task) => {
+    const taskCard = createTaskCard(task);
+    const column = document.querySelector(
+      `.column-div[data-status="${task.status}"] .tasks-container`,
+    );
+
+    if (column) {
+      column.appendChild(taskCard);
+    }
+  });
+
+  updateColumnCounts();
+}
